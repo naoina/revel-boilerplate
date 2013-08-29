78430326679adf11734905684ed388c3b57c6baf
Naoya Inada <naoina@kuune.org>
2013-08-30 01:28:57 +0900

    Add bootstrap.min.js

--- app/assets/js/require_main.js
+++ app/assets/js/require_main.js
@@ -5,10 +5,17 @@ requirejs.config({
     'jquery': [
       '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
       '../bower_components/jquery/jquery.min'
+    ],
+    'bootstrap': [
+      '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min',
+      '../bower_components/bootstrap/dist/js/bootstrap.min'
     ]
   },
   shim: {
+    'bootstrap': ['jquery']
   }
 });
 
-require(['app']);
+require(['jquery'], function () {
+  require(['bootstrap', 'app']);
+});
